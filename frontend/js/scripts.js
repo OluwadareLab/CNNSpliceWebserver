4bcfdb406737e1a68c910fa24354ce55c5a6a4e6
mohit <hmamohit@gmail.com>
2023-04-26 10:50:40 +0600

    fixed file input

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,33 +1,15 @@
-/*!
-* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
 const paste_uri = "";
 const upload_uri = ""
 
-var pastElement = document.getElementById("past-fastq-div");
-var uploadElement = document.getElementById("upload-fastq-div");
-var loginForm = document.getElementById("evalForm");
-var alartBox = document.getElementById("alart-box");
-
 function onChange(val) {
 	if (val == 0) {
-		uploadElement.style.display = "none";
-		pastElement.style.display = "block";
+		document.getElementById("upload-fastq-div").style.display = "none";
+		document.getElementById("past-fastq-div").style.display = "block";
 	}
 	else {
-		pastElement.style.display = "none";
-		uploadElement.style.display = "block";
+		document.getElementById("past-fastq-div").style.display = "none";
+		document.getElementById("upload-fastq-div").style.display = "block";
 	}
-	return;
-}
-
-function backHome() {
-	window.location.replace("./index.html");
 	return;
 }
 
@@ -36,109 +18,17 @@ function showMore() {
 	document.getElementById('more').style.display = "block";
 }
 
-async function readContent(file) {
-	let reader = new FileReader()
-	reader.readAsText(file)
-	await new Promise(resolve => reader.onload = () => resolve())
-	return reader.result
-}
-
-loginForm.addEventListener("submit", async (event) => {
-	event.preventDefault();
-
-	var email = document.getElementById("email");
-	var model = document.getElementById("species");
-	var data = "";
-
-	var status = false;
-	if (document.getElementById('paste-option').checked) {
-		data = document.getElementById('paste-data').value;
-		status = checkFasta(data);
-
-		if (status) {
-			var body = {
-				"email": email.value,
-				"model": model.value,
-				"data": data.value
-			};
-
-			body = JSON.stringify(body);
-			console.log(`${body}`);
-
-			await fetch(paste_uri, {
-				method: "post",
-				body: body
-			}).then(results => {
-				results.json();
-				window.location.replace("./thankyou.html");
-			}).catch((error) => ("Something went wrong!", error));
-
-		} else {
-			document.getElementById("alart-box").style.display = "block";
-		}
-
-	} else if (document.getElementById('upload-option').checked) {
-		var inputFiles = document.getElementById('file');
-		data = await readContent(inputFiles.files[0]);
-		status = checkFasta(data);
-
-		if (status) {
-			const formData = new FormData();
-			formData.append("email", email.value);
-			formData.append("model", model.value);
-			formData.append("data", inputFiles.files[0]);
-
-			console.log(`${JSON.stringify(formData)}`);
-			
-			await fetch(upload_uri, {
-				method: "post",
-				body: formData,
-			}).then(results => {
-				results.json();
-				window.location.replace("./thankyou.html");
-			}).catch((error) => ("Something went wrong!", error));
-
-		} else {
-			document.getElementById("alart-box").style.display = "block";
-		}
-
-	}
-
-	email.value = "";
-	species.selectedIndex = 0;
-
-	return;
-});
-
-function sendRequest() {
-	const xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-	xhr.send();
-	xhr.responseType = "json";
-	xhr.onload = () => {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			const data = xhr.response;
-			console.log(data);
-		} else {
-			console.log(`Error: ${xhr.status}`);
-		}
-	};
-}
-
-function checkFasta(data) {
+function checkFasta(sequenceText) {
 	var countHead = 0;
 	var countSeq = 0;
-	var regex = "^[ATGCatcg]+$";
-	var lines = data.trim().split('\n');
+	var regex = "^[ATGCatcg]{400}$";
+	var lines = sequenceText.trim().split('\n');
 	for (var i = 0; i < lines.length; i++) {
 		var line = lines[i];
 		if (line.startsWith(">")) {
 			countHead++;
 		} else if (countSeq < countHead) {
-			if (line.trim().length != 400) {
-				return false;
-			}
-			if(!regex.match(line)) {
+			if (!line.trim().match(regex)) {
 				return false;
 			}
 			countSeq++;
@@ -153,3 +43,126 @@ function checkFasta(data) {
 
 	return true;
 }
+
+async function readContent(file) {
+	var reader = new FileReader();
+	reader.readAsText(file);
+	await new Promise(resolve => reader.onload = () => {
+		reader.result;
+		resolve();
+	});
+	return reader.result;
+}
+
+/*
+const reader = new FileReader();
+function handleEvent(event) {
+	if (event.type === "load") {
+		document.querySelector("p.preview").value = reader.result;
+	}
+}
+
+function addListeners(reader) {
+	reader.addEventListener("loadstart", handleEvent);
+	reader.addEventListener("load", handleEvent);
+	reader.addEventListener("loadend", handleEvent);
+	reader.addEventListener("progress", handleEvent);
+	reader.addEventListener("error", handleEvent);
+	reader.addEventListener("abort", handleEvent);
+}
+
+function handleSelected(e) {
+	const selectedFile = document.querySelector('input[type="file"]').files[0];
+	if (selectedFile) {
+		addListeners(reader);
+		reader.readAsText(selectedFile);
+	}
+}
+
+document.querySelector('input[type="file"]').addEventListener("change", handleSelected);
+*/
+
+document.getElementById("evalForm").addEventListener("submit", async (event) => {
+	event.preventDefault();
+
+	var email = document.getElementById("email").value;
+	var model = document.getElementById("species").value;
+	console.log('submit');
+
+	var status = false;
+	if (document.getElementById('paste-option').checked) {
+		var sequenceText = document.getElementById('paste-data').value;
+		status = checkFasta(sequenceText);
+
+		if (status) {
+			document.getElementById("alart-box").style.display = "none";
+
+			var body = {
+				"email": email,
+				"model": model,
+				"data": sequenceText
+			};
+
+			body = JSON.stringify(body);
+			console.log(`${body}`);
+
+			await fetch(paste_uri, {
+				method: "post",
+				body: body
+			}).then(results => {
+				results.json();
+				document.getElementById("email").value = "";
+				document.getElementById("species").selectedIndex = 0;
+				window.location.replace("./thankyou.html");
+			}).catch((error) => {
+				console.log("Something went wrong!", error);
+				document.getElementById("email").value = "";
+				document.getElementById("species").selectedIndex = 0;
+				window.location.replace("./error.html");
+			});
+		} else {
+			document.getElementById("alart-box").style.display = "block";
+		}
+
+	} else if (document.getElementById('upload-option').checked) {
+
+		console.log('upload');
+
+		var file = document.getElementById('file').files[0];
+		console.log(file);
+
+		// var sequenceText = document.querySelector("p.preview").value;
+
+		var sequenceText = await readContent(file);
+		console.log(`${sequenceText}`);
+		status = checkFasta(sequenceText);
+
+		if (status) {
+			document.getElementById("alart-box").style.display = "none";
+
+			var formData = new FormData();
+			formData.append("email", email);
+			formData.append("model", model);
+			formData.append("data", file);
+
+			console.log(`${JSON.stringify(formData)}`);
+
+			await fetch(upload_uri, {
+				method: "post",
+				body: formData,
+			}).then(results => {
+				results.json();
+				document.getElementById("email").value = "";
+				document.getElementById("species").selectedIndex = 0;
+				window.location.replace("./thankyou.html");
+			}).catch((error) => {
+				console.log("Something went wrong!", error);
+				document.getElementById("email").value = "";
+				document.getElementById("species").selectedIndex = 0;
+				window.location.replace("./error.html");
+			});
+		} else {
+			document.getElementById("alart-box").style.display = "block";
+		}
+	}
+});
