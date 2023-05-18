f2dd00bf46db3d176664a13dca5f0b537786ef10
mohit <hmamohit@gmail.com>
2023-05-18 14:52:54 +0600

    fixed port

--- frontend/script.sh
+++ frontend/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t cnn_splice_frontend .
-docker run -it -d --name cnn_splice_frontend -p 8080:8080 cnn_splice_frontend
+docker run -it -d --name cnn_splice_frontend -p 80:80 cnn_splice_frontend
 docker images
 docker ps -a
